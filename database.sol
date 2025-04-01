ca534d4ed4bb2b60fe5684908652afae97227e31
Udyamita108 <udyamita108@gmail.com>
2025-04-01 11:27:09 +0530

    Update database.sol

--- database.sol
+++ database.sol
@@ -13,11 +13,9 @@ contract UserDatabase {
     
     event UserRegistered(address indexed wallet, string username);
     event XPUpdated(address indexed wallet, uint newXP);
-    
-    // Register or update a user
+  
     function setUser(string memory _username) external {
         if (users[msg.sender].wallet == address(0)) {
-            // New user
             users[msg.sender] = User({
                 wallet: msg.sender,
                 username: _username,
@@ -26,24 +24,20 @@ contract UserDatabase {
             userAddresses.push(msg.sender);
             emit UserRegistered(msg.sender, _username);
         } else {
-            // Existing user - update username only
             users[msg.sender].username = _username;
         }
     }
-    
-    // Update XP (callable by owner/dashboard)
+  
     function updateXP(address _user, uint _xp) external {
         require(users[_user].wallet != address(0), "User not registered");
         users[_user].xp = _xp;
         emit XPUpdated(_user, _xp);
     }
     
-    // Get user count
     function getUserCount() public view returns (uint) {
         return userAddresses.length;
     }
-    
-    // Get all users (for UI)
+
     function getAllUsers() public view returns (User[] memory) {
         User[] memory allUsers = new User[](userAddresses.length);
         for (uint i = 0; i < userAddresses.length; i++) {
